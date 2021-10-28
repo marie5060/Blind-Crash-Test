60b9727e36c587d163d3882558c059a4d1a5d973
Koldo Etxegarai <etxegaraikoldo@gmail.com>
2021-10-28 12:47:41 +0200

    nav route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import AccueilPage from './components/Accueil/AccueilPage';
+import ThemesPage from './components/Themes/ThemesPage';
+import ResultatsPage from './components/Resultats/ResultatsPage';
+import QuizzPage from './components/Quizz/QuizzPage';
 
 function App() {
   return (
     <div>
+      {/* HEADER */}
       <Header />
+      <Switch>
+        <Route exact path="/" component={AccueilPage} />
+        <Route path="/Themes" component={ThemesPage} />
+        <Route path="/Resultats" component={ResultatsPage} />
+        <Route path="/Quizz" component={QuizzPage} />
+      </Switch>
     </div>
   );
 }
