3f43fa370155ff5dfe678c5727603b71fc6c33ee
Koldo Etxegarai <etxegaraikoldo@gmail.com>
2021-10-28 11:18:13 +0200

    projet ok pour npm start

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,6 @@ function App() {
     <div>
       <Header />
       <AppelApi />
-      <Content />
-      <Footer />
     </div>
   );
 }
--- src/components/Quizz/AppelApi.jsx
+++ src/components/Quizz/AppelApi.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import DisplayMusic from '../DisplayMusic';
+import DisplayMusic from './DisplayMusic';
 
 const sampleMusic = {
   title: 'fff',
