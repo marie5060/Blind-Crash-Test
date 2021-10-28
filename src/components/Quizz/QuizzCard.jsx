b45b08caff590e7301814922bf5c7c4468839639
Louya64 <cixie@live.fr>
2021-10-28 12:03:04 +0200

    add QuizzCard component

--- src/components/Quizz/QuizzCard.jsx
+++ src/components/Quizz/QuizzCard.jsx
@@ -1,19 +1,25 @@
+/* eslint-disable */
 import React from 'react';
 import QuizzAlbumPicture from './QuizzAlbumPicture';
 import QuizzAudio from './QuizzAudio';
 import QuizzAnswerButton from './QuizzAnswerButton';
+import TimerButton from './TimerButton';
+import './QuizzCard.css';
 
-const QuizzCard = () => {
+const QuizzCard = ({ track }) => {
   return (
-    <div>
-      <QuizzAlbumPicture />
-      <QuizzAudio />
-      <div>
-        <QuizzAnswerButton />
+    <div className="quizzCard">
+      <div className="pictureContainer">
+        <QuizzAlbumPicture url={track.album.cover_medium} />
+        <QuizzAudio url={track.preview} />
+      </div>
+      <div className="answerBtnContainer">
+        {/* <QuizzAnswerButton />  //la bonne rep est dans track.title_short */}
         <QuizzAnswerButton />
         <QuizzAnswerButton />
         <QuizzAnswerButton />
       </div>
+      <TimerButton />
     </div>
   );
 };
