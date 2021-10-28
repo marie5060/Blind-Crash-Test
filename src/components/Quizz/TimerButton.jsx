276cb128cd564d08704c88095611540233cc170b
Kelhann2608 <bernardin2608@gmail.com>
2021-10-28 11:18:19 +0200

    files timerButton jsx and css added

--- src/components/Quizz/TimerButton.jsx
+++ src/components/Quizz/TimerButton.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './TimerButton.css';
 
 const TimerButton = () => {
-  return <div>coucou je suis un timer</div>;
+  const [count, setCount] = useState(30);
+
+  React.useEffect(() => {
+    const timer = count > 0 && setInterval(() => setCount(count - 1), 1000);
+    return () => clearInterval(timer);
+  }, [count]);
+
+  return (
+    <div className="timerbutton">
+      Timer {count}
+      <div>
+        <p>Questions x/10</p>
+      </div>
+      <div>
+        <button type="button">Suivant</button>
+      </div>
+    </div>
+  );
 };
 
 export default TimerButton;
