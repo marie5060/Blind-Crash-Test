04a8ec8bee203e85ce08aa9f12b581b2e0c49ed9
Koldo Etxegarai <etxegaraikoldo@gmail.com>
2021-11-03 13:34:47 +0100

    burger nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,37 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import BurgerNav from './BurgerNav/BurgerNav';
 import './Header.css';
 import logo from './logo.gif';
 
 const Header = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const handleShowLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  let active = 'navbar-burger';
+
+  if (showLinks === false) {
+    active += ' not-active';
+  } else {
+    active += ' active';
+  }
+
   return (
-    <header className="header-nav">
-      <div className="header-nav-logo">
-        <img src={logo} alt="logo" />
-      </div>
-      <ul className="header-nav-links">
-        <li>
-          <Link className="header-nav-links-a" to="/">
-            Accueil
-          </Link>
-        </li>
-        <li>
-          <Link className="header-nav-links-a" to="/Themes">
-            Thèmes
-          </Link>
-        </li>
-        <li>
-          <Link className="header-nav-links-a" to="/Resultats">
-            Résultats
-          </Link>
-        </li>
-        <li>
-          <Link className="header-nav-links-a" to="/Quizz">
-            Quizz
-          </Link>
-        </li>
-      </ul>
-    </header>
+    <div className="header">
+      <header className="header-nav">
+        <div className="header-nav-logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <nav className="header-navbar">
+          <ul className="header-nav-links">
+            <li>
+              <Link className="header-nav-links-a" to="/BlindTest/">
+                Accueil
+              </Link>
+            </li>
+            <li>
+              <Link className="header-nav-links-a" to="/BlindTest/Themes">
+                Thèmes
+              </Link>
+            </li>
+            <li>
+              <Link className="header-nav-links-a" to="/BlindTest/Resultats">
+                Résultats
+              </Link>
+            </li>
+            <li>
+              <Link className="header-nav-links-a" to="/BlindTest/Quizz">
+                Quizz
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <button type="button" className={active} onClick={handleShowLinks}>
+          <span className="burger-line" />
+        </button>
+      </header>
+      <BurgerNav showLink={showLinks} />
+    </div>
   );
 };
 
