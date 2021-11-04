ba7fa52d103b709097796beffdf2a9de828db4a3
Louya64 <cixie@live.fr>
2021-11-04 11:19:04 +0100

    add filters, okData

--- src/components/Quizz/QuizzPage.jsx
+++ src/components/Quizz/QuizzPage.jsx
@@ -14,18 +14,85 @@ const QuizzPage = () => {
   useEffect(() => {
     axios
       .get(
-        'https://cors-anywhere.herokuapp.com/https://api.deezer.com/playlist/9631244962?' // céline dion playlist emilie
+        'https://cors-anywhere.herokuapp.com/https://api.deezer.com/playlist/9626980522'
       ) // https://cors-anywhere.herokuapp.com/ à ajouter au début
       .then((response) => response.data.tracks.data)
       .then((data) => {
         setTracks(data);
-        console.log("dataLength " + data.length);
-        const preview = data.filter((obj) => obj.preview);
-        console.log("prevLength " + preview.length);
-        const title_short = data.filter((obj) => obj.title_short);
-        console.log("titleLength " + title_short.length);
-        const picture = data.filter((obj) => obj.album.cover_medium); 
-        console.log("pictureLength " + picture.length);
+        console.log('dataLength ' + data.length);
+        const touteLaDATA = data.map(
+          (track, index) =>
+            (track = {
+              index: index,
+              readable: track.readable,
+              title: track.title_short,
+              artiste: track.artist.name,
+              album: track.album.title,
+              duree: track.duration,
+              picture: track.album.cover_medium,
+              preview: track.preview
+            })
+        )
+        console.log(touteLaDATA);
+        // console.log(
+        //   data.map(
+        //     (track, index) =>
+        //       (track = {
+        //         index: index,
+        //         readable: track.readable,
+        //         title: track.title_short,
+        //         artiste: track.artist.name,
+        //         album: track.album.title,
+        //         duree: track.duration,
+        //       })
+        //   )
+        // );
+
+        //ok data:
+        const okData = touteLaDATA.filter((obj) => obj.picture && obj.preview && !(obj.title).includes('(') && obj.readable);
+        console.log('okDataLength ' + okData.length)
+        if (okData.length > 0) {
+          console.log(
+            okData.map(
+              (track) => track)
+          );
+        }
+
+        //data à sortir
+        // const pasOKdata = touteLaDATA.filter((obj) => !obj.album.cover_medium || !obj.preview || (obj.title_short).includes('(') || !obj.readable);
+        // console.log('pasOKdataLength ' + pasOKdata.length)
+        // if (pasOKdata.length > 0) {
+        //   console.log(
+        //     pasOKdata.map(
+        //       (track) =>
+        //         (track = {
+        //           index: track.index,
+        //           readable: track.readable,
+        //           title: track.title_short,
+        //           artiste: track.artist.name,
+        //           album: track.album.title,
+        //           duree: track.duration,
+        //         })
+        //     )
+        //   );
+        // }
+        
+        // const preview = data.filter((obj) => obj.preview);
+        // console.log('prevLength ' + preview.length);
+
+        // const picture = data.filter((obj) => obj.album.cover_medium);
+        // console.log('pictureLength ' + picture.length);
+        
+        // const containBracket = data.filter((obj) =>
+        //   (obj.title_short).includes('(')
+        // );
+        // console.log('bracketsLength ' + containBracket.length);
+
+        // const noPreview = data.filter((obj) => !obj.preview);
+        // console.log('noPreviewLength ' + noPreview.length);
+
+        // const notReadable = data.filter((obj) => obj.readable === false);
+        // console.log('notReadableLength ' + notReadable.length);
       });
   }, []);
 
