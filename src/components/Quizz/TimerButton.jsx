ee37b019a8fada2d52efd8073f26044e507cd990
Kelhann2608 <bernardin2608@gmail.com>
2021-11-04 09:21:39 +0100

    file modified

--- src/components/Quizz/TimerButton.jsx
+++ src/components/Quizz/TimerButton.jsx
@@ -12,19 +12,6 @@ const TimerButton = () => {
       leftTime > 0 && setInterval(() => setLeftTime(leftTime - 0.1), 30);
     return () => clearInterval(timer);
   }, [leftTime]);
-  // const element = document.getElementById('progressBar');
-  // const timeleft = 30;
-  // const timetotal = 30;
-  // const progressBarWidth = (timeleft * element.offsetWidth) / timetotal;
-  // element
-  //   .find('div')
-  //   .animate({ width: progressBarWidth }, 500)
-  //   .html(`${Math.floor(timeleft / 60)}:${timeleft % 60}`);
-  // if (timeleft > 0) {
-  //   setTimeout(() => {
-  //     TimerButton(timeleft - 1, timetotal, element);
-  //   }, 1000);
-  // }
   return (
     <div id="progressBar">
       <div className="bar" style={timerStyle} />
