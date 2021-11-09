435c6311dd38c5a6f19fa3d6a7d6a943348362e9
aurelienbrethes <aurelienbrethes@gmail.com>
2021-11-09 16:03:56 +0100

    fix bugs

--- src/components/Themes/ThemesPage.jsx
+++ src/components/Themes/ThemesPage.jsx
@@ -5,31 +5,31 @@ import './ThemesPage.css';
 
 const ThemesPage = ({ setChosenId }) => {
   const themeArray = [
-    { name: 'Rock', id: '9626980522', num: 0 },
-    { name: 'reggae ', id: '9626990642', num: 1 },
-    { name: 'Métal', id: '9626971702', num: 2 },
-    { name: 'Années 70/80', id: '9640482882', num: 3 },
-    { name: 'Années 90/00', id: '9640491642', num: 4 },
-    { name: 'Génériques', id: '9640499662', num: 5 },
-    { name: 'Chansons Paillardes', id: '9640504822', num: 6 },
-    { name: 'Hymnes', id: '9640511622', num: 7 },
+    { name: 'Rock', id: '9626980522', num: 1 },
+    { name: 'reggae ', id: '9626990642', num: 2 },
+    { name: 'Métal', id: '9626971702', num: 3 },
+    { name: 'Années 70/80', id: '9640482882', num: 4 },
+    { name: 'Années 90/00', id: '9640491642', num: 5 },
+    { name: 'Génériques', id: '9640499662', num: 6 },
+    { name: 'Chansons Paillardes', id: '9640504822', num: 7 },
+    { name: 'Hymnes', id: '9640511622', num: 8 },
   ];
 
   const artisteArray = [
-    { name: 'Céline Dion', id: '9626980522', num: 0 },
-    { name: 'Johnny Halliday ', id: '9626990642', num: 1 },
-    { name: 'Francis Cabrel', id: '9626971702', num: 2 },
-    { name: 'Queen', id: '9640482882', num: 3 },
-    { name: 'Beatles', id: '9640491642', num: 4 },
-    { name: 'Daft Punk', id: '9640499662', num: 5 },
-    { name: 'Metallica', id: '9640504822', num: 6 },
-    { name: 'Michael Jackson', id: '9640511622', num: 7 },
+    { name: 'Céline Dion', id: '9626980522', num: 1 },
+    { name: 'Johnny Halliday ', id: '9626990642', num: 2 },
+    { name: 'Francis Cabrel', id: '9626971702', num: 3 },
+    { name: 'Queen', id: '9640482882', num: 4 },
+    { name: 'Beatles', id: '9640491642', num: 5 },
+    { name: 'Daft Punk', id: '9640499662', num: 6 },
+    { name: 'Metallica', id: '9640504822', num: 7 },
+    { name: 'Michael Jackson', id: '9640511622', num: 8 },
   ];
 
   return (
     <div className="themes-page-container">
       <h2>Thèmes</h2>
-      <div className="themeItems">
+      <div className="theme-items">
         {themeArray.map((item) => (
           <div className={`item${item.num} item`}>
             <ThemeItem
@@ -42,7 +42,7 @@ const ThemesPage = ({ setChosenId }) => {
         ))}
       </div>
       <h2>Artistes</h2>
-      <div className="themeItems">
+      <div className="theme-items">
         {artisteArray.map((item) => (
           <div className={`item${item.num} item`}>
             <ThemeItem
