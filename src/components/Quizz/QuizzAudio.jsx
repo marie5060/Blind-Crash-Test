fedb2b552b86e8283671350ddaf54d95aff78f29
Bernardin <bernardin2608@gmail.com>
2021-11-10 10:33:51 +0100

    probleme fixed

--- src/components/Quizz/QuizzAudio.jsx
+++ src/components/Quizz/QuizzAudio.jsx
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-/* eslint-disable */
-=======
-/*eslint-disable */
->>>>>>> bd79e0e7e3f06e46ec7a04b639c36509e3411583
-=======
->>>>>>> 938deed7bfa0a0267e8ae6b7b880656f8296e5f8
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactPlayer from 'react-player';
