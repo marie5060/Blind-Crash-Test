93f91431465232dbf334a88f8038e40bfe25efda
Louya64 <cixie@live.fr>
2021-11-12 09:57:22 +0100

    states in app => to ResultatsPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
@@ -10,6 +10,20 @@ import './App.css';
 
 function App() {
   const [chosenId, setChosenId] = useState('9626980522');
+  const [scoreFinal, setScoreFinal] = useState(0);
+  const [pseudo, setPseudo] = useState('Anonyme');
+  const [themeName, setThemeName] = useState('Aucun thème');
+  const [difficulty, setDifficulty] = useState(2);
+  const [quizzResult, setQuizzResult] = useState({});
+
+  useEffect(() => {
+    setQuizzResult({
+      scoreFinal,
+      pseudo,
+      themeName,
+      difficulty,
+    });
+  }, [scoreFinal]);
 
   return (
     <div>
@@ -19,19 +33,29 @@ function App() {
           <Route
             exact
             path="/Blind-Crash-Test/"
-            component={() => <AccueilPage setChosenId={setChosenId} />}
+            component={() => (
+              <AccueilPage setChosenId={setChosenId} setPseudo={setPseudo} />
+            )}
           />
           <Route
             path="/Blind-Crash-Test/Themes"
-            component={() => <ThemesPage setChosenId={setChosenId} />}
+            component={() => (
+              <ThemesPage
+                setChosenId={setChosenId}
+                setThemeName={setThemeName}
+                setDifficulty={setDifficulty}
+              />
+            )}
           />
           <Route
             path="/Blind-Crash-Test/Resultats"
-            component={() => <ResultatsPage setChosenId={setChosenId} />}
+            component={() => <ResultatsPage quizzResult={quizzResult} />}
           />
           <Route
             path="/Blind-Crash-Test/Quizz"
-            component={() => <QuizzPage chosenId={chosenId} />}
+            component={() => (
+              <QuizzPage chosenId={chosenId} setScoreFinal={setScoreFinal} />
+            )}
           />
         </Switch>
       </BrowserRouter>
--- src/components/Resultats/ResultatsPage.jsx
+++ src/components/Resultats/ResultatsPage.jsx
@@ -1,7 +1,75 @@
-const ResultatsPage = () => {
+import PropTypes from 'prop-types';
+import Stars from '../Themes/Difficulty';
+import './ResultatsPage.css';
+
+const ResultatsPage = ({ quizzResult }) => {
+  const starsQte = [];
+  for (let i = 0; i < quizzResult.difficulty; i += 1) {
+    starsQte.push(i);
+  }
+
   return (
-    <div className="ResultatsPage-Container">Coucou je suis le resultats</div>
+    <main>
+      <div className="result-gauge">
+        Méga
+        mauvais=============================|==================================Méga
+        Bon
+      </div>
+      <div className="result-table-container">
+        <h1 className="result-title">Tableau des scores</h1>
+        <table className="result-table">
+          <thead>
+            <tr>
+              <th>Score</th>
+              <th>Pseudo</th>
+              <th>Thème</th>
+              <th>Difficulté</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{quizzResult.scoreFinal}</td>
+              <td>{quizzResult.pseudo}</td>
+              <td>{quizzResult.themeName}</td>
+              <td className="result-stars-container">
+                {starsQte.map((elem) => (
+                  <Stars key={elem} />
+                ))}
+              </td>
+            </tr>
+            <tr>
+              <td>10 000</td>
+              <td>Aurélien</td>
+              <td>Céline Dion</td>
+              <td className="result-stars-container">
+                <Stars />
+                <Stars />
+                <Stars />
+                <Stars />
+                <Stars />
+              </td>
+            </tr>
+            <tr>
+              <td>10 000</td>
+              <td>Aurélien</td>
+              <td>Céline Dion</td>
+              <td className="result-stars-container">
+                <Stars />
+                <Stars />
+                <Stars />
+                <Stars />
+                <Stars />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
   );
 };
 
 export default ResultatsPage;
+
+ResultatsPage.propTypes = {
+  quizzResult: PropTypes.oneOfType([PropTypes.object]).isRequired,
+};
