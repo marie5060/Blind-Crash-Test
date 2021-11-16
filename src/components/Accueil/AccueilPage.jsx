e4b5bf8c70788e628cf47beb59d8db1570d80739
Koldo Etxegarai <etxegaraikoldo@gmail.com>
2021-11-16 16:17:07 +0100

    home design

--- src/components/Accueil/AccueilPage.jsx
+++ src/components/Accueil/AccueilPage.jsx
@@ -8,22 +8,24 @@ const AccueilPage = ({ setChosenId, setChosenTheme }) => {
     <main>
       <div className="pseudo-container">
         <h1>Choisis ton pseudo avant de lancer ta partie</h1>
-        <form>
+        <form className="form-container">
           <input className="pseudo-input" type="text" placeholder="Pseudo" />
         </form>
       </div>
       <div className="fast-game-container">
-        <div>
+        <div className="fast-game-container-text">
           <h1>Jouer immédiatement !</h1>
           <p>
             Envie de hasard et de tester tes connaissances, lance une partie
             aléatoire !
           </p>
         </div>
-        <LinkBtnFastGame
-          setChosenId={setChosenId}
-          setChosenTheme={setChosenTheme}
-        />
+        <div className="fast-game-container-button">
+          <LinkBtnFastGame
+            setChosenId={setChosenId}
+            setChosenTheme={setChosenTheme}
+          />
+        </div>
       </div>
       <div className="new-theme-container">
         <h1> Personnaliser une partie</h1>
